b8f0d2edbc301e72d00785166ce8fc7cea4028e6
liron <liron>
2023-04-27 18:56:30 +0300

    fixtures in backend

--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -1,9 +1,12 @@
 import datetime
+import json
 import re
 import mongomock
 from fastapi.testclient import TestClient
+from pydantic import BaseModel
 import pytest
-from shared.models import ResultModel
+from backend.models import UpdateMonitorModel
+from shared.models import MonitorModel, ResultModel
 
 from shared.mongo import MONGO_DB_NAME, MONITORS_COLLECTION_NAME, get_prod_client
 
@@ -25,6 +28,39 @@ def run_around_tests(requests_mock):
     yield
 
 
+@pytest.fixture()
+def example_monitor_id():
+    return "666f6f2d6261722d71757578"
+
+
+@pytest.fixture()
+def example_monitor(example_monitor_id):
+    monitor = MonitorModel(
+        description="this is a test monitor",
+        url="http://httpbin.org/post",
+        method="POST",
+        body="{\"hello\":\"world\"}",
+        expected_status=200,
+        results=[ResultModel(status=200, time=datetime.datetime.now(), content='wow',
+                             duration_ms=5000, headers={})]
+    ).dict()
+
+    monitor['_id'] = example_monitor_id
+
+    return monitor
+
+
+@pytest.fixture()
+def example_update_monitor():
+    return UpdateMonitorModel(
+        description="updated_description",
+        url="http://httpbin.org/get",
+        method="GET",
+        body="",
+        expected_status=404
+    ).dict()
+
+
 def test_now_allowed():
     client = TestClient(app)
     response = client.post('/monitors/getmonitors/false')
@@ -32,95 +68,58 @@ def test_now_allowed():
     assert response.status_code == 405
 
 
-def test_with_results():
+@pytest.mark.parametrize("with_results,results_len", [(False, 0), (True, 1)])
+def test_with_results(example_monitor, with_results, results_len):
     db = mongo_test_client[MONGO_DB_NAME]
     collection = db[MONITORS_COLLECTION_NAME]
-    monitor_id = "666f6f2d6261722d71757578"
-    collection.insert_one({
-        "description": "this is a test monitor",
-        "url": "http://httpbin.org/post",
-        "method": "POST",
-        "body": "{\"hello\":\"world\"}",
-        "expected_status": 200,
-        "_id": monitor_id,
-        'results': [ResultModel(status=200, time=datetime.datetime.now(), content='wow',
-                                duration_ms=5000, headers={}).dict()]
-    })
+    collection.insert_one(example_monitor)
 
     client = TestClient(app)
-    response = client.get('/monitors/getmonitors/false')
+    response = client.get(f'/monitors/getmonitors/{with_results}')
     result = response.json()
 
     assert response.status_code == 200
     assert len(result) == 1
-    assert len(result[0]['results']) == 0
-
-    response = client.get('/monitors/getmonitors/true')
-    result = response.json()
-
-    assert response.status_code == 200
-    assert len(result) == 1
-    assert len(result[0]['results']) == 1
+    assert len(result[0]['results']) == results_len
 
 
-def test_update_nonexistent():
+def test_update_nonexistent(example_update_monitor):
     nonexistent_id = '12345678'
 
-    data = {
-        "description": "this is a test monitor",
-        "url": "http://httpbin.org/post",
-        "method": "POST",
-        "body": "{\"hello\":\"world\"}",
-        "expected_status": 200,
-    }
-
     client = TestClient(app)
-    response = client.put(f'/monitors/{nonexistent_id}', json=data)
+    response = client.put(
+        f'/monitors/{nonexistent_id}', json=example_update_monitor)
 
     assert response.status_code == 404
     assert response.json()['detail'] == f"Monitor {nonexistent_id} not found"
 
 
-def test_update():
+def test_update(example_monitor, example_monitor_id, example_update_monitor):
     db = mongo_test_client[MONGO_DB_NAME]
     collection = db[MONITORS_COLLECTION_NAME]
-    monitor_id = "666f6f2d6261722d71757578"
-    collection.insert_one({
-        "description": "this is a test monitor",
-        "url": "http://httpbin.org/post",
-        "method": "POST",
-        "body": "{\"hello\":\"world\"}",
-        "expected_status": 200,
-        "_id": monitor_id
-    })
+    collection.insert_one(example_monitor)
 
     client = TestClient(app)
 
-    updated_data = {
-        "description": "updated_description",
-        "url": "http://httpbin.org/get",
-        "method": "GET",
-        "body": "",
-        "expected_status": 404,
-    }
-
-    response = client.put(f'/monitors/{monitor_id}', json=updated_data)
+    response = client.put(
+        f'/monitors/{example_monitor_id}', json=example_update_monitor)
 
     assert response.status_code == 200
 
     result = response.json()
 
-    for field in updated_data:
-        assert result[field] == updated_data[field]
+    for field in example_update_monitor:
+        assert result[field] == (
+            example_monitor[field] if example_update_monitor[field] is None else example_update_monitor[field])
 
 
-def test_id_not_found():
-    nonexistent_id = '12345678'
+def test_id_not_found(example_monitor_id):
     client = TestClient(app)
-    response = client.get(f'/monitors/{nonexistent_id}')
+    response = client.get(f'/monitors/{example_monitor_id}')
 
     assert response.status_code == 404
-    assert response.json()['detail'] == f"Monitor {nonexistent_id} not found"
+    assert response.json()[
+        'detail'] == f"Monitor {example_monitor_id} not found"
 
 
 def test_get_monitors_empty():
@@ -131,51 +130,42 @@ def test_get_monitors_empty():
     assert response.json() == []
 
 
-def test_create_monitor():
+def test_create_monitor(example_monitor):
+    del example_monitor['id']
+    del example_monitor['results']
+
     client = TestClient(app)
-
-    data = {
-        "description": "this is a test monitor",
-        "url": "http://httpbin.org/post",
-        "method": "POST",
-        "body": "{\"hello\":\"world\"}",
-        "expected_status": 200,
-    }
-
-    response = client.post(f'/monitors', json=data)
+    response = client.post(
+        f'/monitors', json=json.loads(json.dumps(example_monitor, default=str)))
 
     assert response.status_code == 201
 
     result = response.json()
 
-    for field in data:
-        assert result[field] == data[field]
+    for field in example_monitor:
+        assert result[field] == example_monitor[field]
 
 
-def test_get_monitor():
+def test_get_monitor(example_monitor, example_monitor_id):
     db = mongo_test_client[MONGO_DB_NAME]
     collection = db[MONITORS_COLLECTION_NAME]
-    monitor_id = "666f6f2d6261722d71757578"
-    collection.insert_one(
-        {"url": "http://httpbin.org/get", "method": "GET", "_id": monitor_id})
+    collection.insert_one(example_monitor)
 
     client = TestClient(app)
 
-    response = client.get(f'/monitors/{monitor_id}')
+    response = client.get(f'/monitors/{example_monitor_id}')
 
     assert response.status_code == 200
-    assert response.json()['_id'] == monitor_id
+    assert response.json()['_id'] == example_monitor_id
 
 
-def test_delete_monitor():
+def test_delete_monitor(example_monitor, example_monitor_id):
     db = mongo_test_client[MONGO_DB_NAME]
     collection = db[MONITORS_COLLECTION_NAME]
-    monitor_id = "666f6f2d6261722d71757578"
-    collection.insert_one(
-        {"url": "http://httpbin.org/get", "method": "GET", "_id": monitor_id})
+    collection.insert_one(example_monitor)
 
     client = TestClient(app)
 
-    response = client.delete(f'/monitors/{monitor_id}')
+    response = client.delete(f'/monitors/{example_monitor_id}')
 
     assert response.status_code == 204
